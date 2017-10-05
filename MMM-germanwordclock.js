ac9a35ad96f7a91acac4719652f15dec3cdb84b1
alexBeuth <alexBeuth@users.noreply.github.com>
2017-10-05 22:58:04 +0200

    Changed to german

--- MMM-germanwordclock.js
+++ MMM-germanwordclock.js
@@ -1,13 +1,13 @@
-/* Word Clock written in the Swiss German dialect of Bern */
+/* Word Clock written in German */
 
 /* Magic Mirror
- * Module: Bern Word Clock
+ * Module: German Word Clock
  *
- * By Sebastian Plattner https://www.sebastianplattner.ch
+ * Original bern dialect version by Sebastian Plattner https://www.sebastianplattner.ch
  * MIT Licensed.
  */
 
-Module.register("MMM-bernwordclock",{
+Module.register("MMM-germanwordclock",{
 
 	// Define module defaults
 	defaults: {
@@ -17,7 +17,7 @@ Module.register("MMM-bernwordclock",{
 
 	// Define required scripts.
 	getStyles: function() {
-		return ["bernwordclock.css"];
+		return ["germanwordclock.css"];
 	},
 
 	// Define required scripts.
@@ -34,30 +34,31 @@ Module.register("MMM-bernwordclock",{
 
 		this.elements = [];
 
-		this.elements["hour1"] = "eis";
-		this.elements["hour2"] = "zwöi";
-		this.elements["hour3"] = "drü";
-		this.elements["hour4"] = "vieri";
-		this.elements["hour5"] = "füfi";
-		this.elements["hour6"] = "sächsi";
-		this.elements["hour7"] = "sibni";
-		this.elements["hour8"] = "achti";
-		this.elements["hour9"] = "nüni";
-		this.elements["hour10"] = "zäni";
-		this.elements["hour11"] = "eufi";
-		this.elements["hour12"] = "zwöufi";
+		this.elements["hour1"] = "hour1";
+		this.elements["hour2"] = "hour2";
+		this.elements["hour3"] = "hour3";
+		this.elements["hour4"] = "hour4";
+		this.elements["hour5"] = "hour5";
+		this.elements["hour6"] = "hour6";
+		this.elements["hour7"] = "hour7";
+		this.elements["hour8"] = "hour8";
+		this.elements["hour9"] = "hour9";
+		this.elements["hour10"] = "hour10";
+		this.elements["hour11"] = "hour11";
+		this.elements["hour12"] = "hour12";
 
-		this.elements["befor"] = "vor";
-		this.elements["past"] = "ab";
+		this.elements["befor"] = "befor";
+		this.elements["past"] = "past";
 
-		this.elements["five"] = "füf";
-		this.elements["ten"] = "zää";
-		this.elements["quarter"] = "viertu";
-		this.elements["twenty"] = "zwänzg";
-		this.elements["half"] = "haubi";
+		this.elements["full"] = "full";
+		this.elements["five"] = "five";
+		this.elements["ten"] = "ten";
+		this.elements["quarter"] = "quarter";
+		this.elements["twenty"] = "twenty";
+		this.elements["half"] = "half";
 
-		this.elements["it"] = "es";
-		this.elements["is"] = "isch";
+		this.elements["it"] = "it";
+		this.elements["is"] = "is";
 
 		this.elements["dot1"] = "dot1";
 		this.elements["dot2"] = "dot2";
@@ -83,7 +84,7 @@ Module.register("MMM-bernwordclock",{
 		var minute = currentTime.minute();
 
 
-		if (minute >= 0 && minute < 5){ }
+		if (minute >= 0 && minute < 5){ elements.push("full");}
 		if (minute >= 5 && minute < 10) { elements.push("five","past"); }
 		if (minute >= 10 && minute < 15) { elements.push("ten","past"); }
 		if (minute >= 15 && minute < 20) { elements.push("quarter","past"); }
@@ -209,20 +210,25 @@ Module.register("MMM-bernwordclock",{
 	// Override dom generator.
 	getDom: function() {
 
-		var wrapper = document.createElement("div");	
+		var wrapper = document.createElement("div");
+        // 11 letters per line
 		wrapper.innerHTML = "<span id=\"dot1\">.</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span id=\"dot2\">.</span><br />" + 
-								"&nbsp;<span id=\"es\">E S</span> K <span id=\"isch\"> I S C H </span> A <span id=\"füf\">F Ü F</span>&nbsp;<br />" +
-								"&nbsp;<span id=\"viertu\">V I E R T U</span> B F <span id=\"zää\" >Z Ä Ä</span>&nbsp;<br />" +
-								"&nbsp;<span id=\"zwänzg\">Z W Ä N Z G</span> S I <span id=\"vor\">V O R</span>&nbsp;<br />" +
-								"&nbsp;<span id=\"ab\">A B </span>O <span id=\"haubi\">H A U B I </span>E G E&nbsp;<br />" +
-								"&nbsp;<span id=\"eis\">E I S </span><span id=\"zwöi\">Z W Ö I </span>S <span id=\"drü\">D R Ü</span>&nbsp;<br />" +
-								"&nbsp;<span id=\"vieri\">V I E R I</span> <span id=\"füfi\">F Ü F I </span>Q T&nbsp;<br />" +
-								"&nbsp;<span id=\"sächsi\">S Ä C H S I </span><span id=\"sibni\">S I B N I</span>&nbsp;<br />" +
-								"&nbsp;<span id=\"achti\">A C H T I </span><span id=\"nüni\">N Ü N I </span>E L&nbsp;<br />" +
-								"&nbsp;<span id=\"zäni\">Z Ä N I </span>E R B <span id=\"eufi\">E U F I</span>&nbsp;<br />" +
-								"&nbsp;<span id=\"zwöufi\">Z W Ö U F I </span>N A U H R&nbsp;<br />" +
+								"&nbsp;<span id=\"it\">E S</span> K<span id=\"is\"> I S T </span> A <span id=\"five\">F Ü N F</span>&nbsp;<br />" +
+								"&nbsp;<span id=\"ten\">Z E H N</span> <span id=\"quarter\">V I E R T E L</span>&nbsp;<br />" +
+								"&nbsp;<span id=\"twenty\">Z W A N Z I G </span>S P Ä T&nbsp;<br />" +
+                                "&nbsp;<span id=\"befor\">V O R</span> N U L L <span id=\"past\">N A C H</span>&nbsp;<br />" +
+                                "&nbsp;<span id=\"half\">H A L B </span> T A U <span id=\"hour1\">E I N S</span>&nbsp;<br />" +
+                                "&nbsp;<span id=\"hour4\">V I E R</span> S T <span id=\"hour12\">Z W Ö L F </span>&nbsp;<br />" +
+								"&nbsp;<span id=\"hour6\">S E C H S </span><span id=\"hour7\">S I E B E N</span>&nbsp;<br />" +
+								"&nbsp;<span id=\"hour3\">D R E I </span><span id=\"hour5\">F Ü N F </span><span id=\"hour11\">E L F</span>&nbsp;<br />" +
+                                "&nbsp;<span id=\"hour9\">N E U N </span>P I <span id=\"hour2\">Z W E I</span>&nbsp;<br />" +
+                                "&nbsp;<span id=\"hour8\">A C H T </span><span id=\"hour10\">Z E H N</span><span id=\"full\">U H R</span>&nbsp;<br />" +
 								"<span id=\"dot3\">.</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span id=\"dot4\">.</span><br />";
 
 		return wrapper;
 	}
 });
+
+
+
+
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Module: Word Clock
-This Magic Mirror module displays a Word Clock written in the Swiss German dialect of Bern.
+This Magic Mirror module displays a Word Clock written in german.
 
 ## Using the module
 
@@ -7,12 +7,9 @@ To use this module, add it to the modules array in the `config/config.js` file:
 ````javascript
 modules: [
     {
-		module: 'MMM-bernwordclock',
+		module: 'MMM-germanwordclock',
 		position: 'bottom_left',
 	},
 ]
 ````
 
-## Screenshot 
-
-![](wordclock.png)
