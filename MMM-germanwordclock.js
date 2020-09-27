3fcf19c1e8de398ce70441f8dfcbe61ae48d88bb
nikolasdas <mail@nikolasdas.de>
2020-09-27 17:23:27 +0100

    first commit

--- MMM-germanwordclock.js
+++ MMM-germanwordclock.js
@@ -3,203 +3,96 @@
 /* Magic Mirror
  * Module: German Word Clock
  *
+ * Fork from alexBeuth https://github.com/alexBeuth/MMM-germanwordclock
  * Original bern dialect version by Sebastian Plattner https://www.sebastianplattner.ch
  * MIT Licensed.
  */
 
-Module.register("MMM-germanwordclock",{
+Module.register('MMM-germanwordclock', {
+    defaults: {
+        updateInterval: 1000,
+        bold: true,
+        italic: false,
+        fontFamily: 'Courier',
+        fontSize: '.5rem',
+        letterSpacing: '1rem',
+        lineHeight: '1rem',
+        color: 'rgba(255,255,255,0.2)',
+        selectedColor: 'rgba(255,255,255,1)'
+    },
 
-	// Define module defaults
-	defaults: {
-		updateInterval: 1000,
-		
-	},
+    getScripts() {
+        return ['moment.js']
+    },
 
-	// Define required scripts.
-	getStyles: function() {
-		return ["germanwordclock.css"];
-	},
+    start() {
+        Log.info('Starting module: ' + this.name)
+        moment.locale(config.language)
+        this.elements = ['hour1', 'hour2', 'hour3', 'hour4', 'hour5', 'hour6', 'hour7', 'hour8', 'hour9', 'hour10', 'hour11', 'hour12', 'before', 'past', 'full', 'five', 'ten', 'quarter', 'twenty', 'half', 'it', 'is', 'einS']
 
-	// Define required scripts.
-	getScripts: function() {
-		return ["moment.js"];
-	},
+        let self = this
+        setInterval(self.updateWordClock, this.config.updateInterval)
+    },
 
-	// Define start sequence.
-	start: function() {
-		Log.info("Starting module: " + this.name);
+    updateWordClock() {
+        const elements = ['it', 'is']
 
-		// Set locale.
-		moment.locale(config.language);
+        const minute = moment().minute()
+        const hour = (minute < 30 ? moment().hour() : moment().hour() + 1) % 12
 
-		this.elements = [];
+        if (minute < 5)
+            elements.push('full')
+        else if (minute < 10)
+            elements.push('five', 'past')
+        else if (minute < 15)
+            elements.push('ten', 'past')
+        else if (minute < 20)
+            elements.push('quarter', 'past')
+        else if (minute < 25)
+            elements.push('twenty', 'past')
+        else if (minute < 30)
+            elements.push('five', 'before', 'half')
+        else if (minute < 35)
+            elements.push('half')
+        else if (minute < 40)
+            elements.push('five', 'past', 'half')
+        else if (minute < 45)
+            elements.push('twenty', 'before')
+        else if (minute < 50)
+            elements.push('quarter', 'before')
+        else if (minute < 55)
+            elements.push('ten', 'before')
+        else
+            elements.push('five', 'before')
+        
+        if (minute >= 4 && hour == 1)
+            elements.push('einS')
 
-		this.elements["hour1"] = "hour1";
-		this.elements["hour2"] = "hour2";
-		this.elements["hour3"] = "hour3";
-		this.elements["hour4"] = "hour4";
-		this.elements["hour5"] = "hour5";
-		this.elements["hour6"] = "hour6";
-		this.elements["hour7"] = "hour7";
-		this.elements["hour8"] = "hour8";
-		this.elements["hour9"] = "hour9";
-		this.elements["hour10"] = "hour10";
-		this.elements["hour11"] = "hour11";
-		this.elements["hour12"] = "hour12";
+        elements.push(hour == 0 ? 'hour12' : `hour${hour}`)
 
-		this.elements["befor"] = "befor";
-		this.elements["past"] = "past";
-
-		this.elements["full"] = "full";
-		this.elements["five"] = "five";
-		this.elements["ten"] = "ten";
-		this.elements["quarter"] = "quarter";
-		this.elements["twenty"] = "twenty";
-		this.elements["half"] = "half";
-
-		this.elements["it"] = "it";
-		this.elements["is"] = "is";
-
-                // Adds the char 's' to 'ein'
-                this.elements["einS"] = "einS"
-
-		// Set Interval for Update
-		var self = this;
-		setInterval(function() {
-			self.updateWordClock();
-		}, this.config.updateInterval);
-	},
-
-	updateWordClock: function() {
-
-		this.resetWordClock();
-
-    	        var currentTime = moment();
-    	        var elements = ["it","is"];
-
-		var hour = currentTime.hour() % 12;
-		var minute = currentTime.minute();
-
-		if (minute >= 0 && minute < 5) { elements.push("full");}
-		if (minute >= 5 && minute < 10) { elements.push("five","past"); }
-		if (minute >= 10 && minute < 15) { elements.push("ten","past"); }
-		if (minute >= 15 && minute < 20) { elements.push("quarter","past"); }
-		if (minute >= 20 && minute < 25) { elements.push("twenty","past"); }
-		if (minute >= 25 && minute < 30) {
-			elements.push("five","befor","half");
-			hour = (hour + 1) % 12;
-		}
-
-		if (minute >= 30 && minute < 35) {
-			elements.push("half");
-			hour = (hour + 1) % 12;
-		}
-
-		if (minute >= 35 && minute < 40) {
-			elements.push("five","past","half");
-			hour = (hour + 1) % 12;
-		}
-
-		if (minute >= 40 && minute < 45) {
-			elements.push("twenty","befor");
-			hour = (hour + 1) % 12;
-		}
-
-		if (minute >= 45 && minute < 50) {
-			elements.push("quarter","befor");
-			hour = (hour + 1) % 12;
-		}
-
-		if (minute >= 50 && minute < 55) {
-			elements.push("ten","befor");
-			hour = (hour + 1) % 12;
-		}
-
-		if (minute >= 55 ) {
-			elements.push("five","befor");
-			hour = (hour + 1) % 12;
-		}
-
-                // When we are using 'eins', add the 's' to 'ein'.
-                // Omit the 's', when we are saying 'es ist ein uhr'.
-                if (minute >= 4 && hour == 1) {
-                    elements.push("einS");
-                }
-
-
-		elements.push(this.setHour(hour));
-
-		this.changeToAchtive(elements);
-	}, 
-	
-	setHour: function(hour) {
-
-		switch(hour) {
-			case 0:
-				return "hour12";
-			case 1:
-				return "hour1";
-			case 2:
-				return "hour2";
-			case 3:
-				return "hour3";
-			case 4:
-				return "hour4";
-			case 5:
-				return "hour5";
-			case 6:
-				return "hour6";
-			case 7:
-				return "hour7";
-			case 8:
-				return "hour8";
-			case 9:
-				return "hour9";
-			case 10:
-				return "hour10";
-			case 11:
-				return "hour11";
-			case 12:
-				return "hour12";
-		}
-	},
-
-	resetWordClock: function() {
-
-		for (var i in this.elements) {
-			var item = document.getElementById(this.elements[i]);
-			item.className = "";
-		}
-
-	},
-
-	changeToAchtive: function(elements) {
-		for (var i in elements) {
-			var item = document.getElementById(this.elements[elements[i]]);
-			item.className = "white";
-		}
-	},
+        for (let el of this.elements)
+            document.getElementById(el).style.color = el in elements ? this.config.selectedColor : null
+    },
     
-	// Override dom generator.
-	getDom: function() {
-
-		var wrapper = document.createElement("div");
-        // 11 letters per line
-		wrapper.innerHTML = "&nbsp;<span id=\"it\">E S</span> K<span id=\"is\"> I S T </span> A <span id=\"five\">K U R Z</span>&nbsp;<br />" +
-								"&nbsp;<span id=\"ten\">Z E H N</span> <span id=\"quarter\">V I E R T E L</span>&nbsp;<br />" +
-								"&nbsp;<span id=\"twenty\">Z W A N Z I G </span>S P Ä T&nbsp;<br />" +
-                                "&nbsp;<span id=\"befor\">V O R</span> N U L L <span id=\"past\">N A C H</span>&nbsp;<br />" +
-                                "&nbsp;<span id=\"half\">H A L B </span> T A U <span id=\"hour1\">E I N </span><span id=\"einS\">S</span>&nbsp;<br />" +
-                                "&nbsp;<span id=\"hour4\">V I E R</span> S T <span id=\"hour12\">Z W Ö L F </span>&nbsp;<br />" +
-								"&nbsp;<span id=\"hour6\">S E C H S </span><span id=\"hour7\">S I E B E N</span>&nbsp;<br />" +
-								"&nbsp;<span id=\"hour3\">D R E I </span><span id=\"hour5\">F Ü N F </span><span id=\"hour11\">E L F</span>&nbsp;<br />" +
-                                "&nbsp;<span id=\"hour9\">N E U N </span>P I <span id=\"hour2\">Z W E I</span> N&nbsp;<br />" +
-                                "&nbsp;<span id=\"hour8\">A C H T </span><span id=\"hour10\">Z E H N </span><span id=\"full\">U H R</span>&nbsp;<br />";
-
-		return wrapper;
-	}
-});
-
-
-
-
+    getDom() {
+        const wrapper = document.createElement('div')
+        wrapper.style.color = this.config.color
+        wrapper.style.fontSize = this.config.fontSize
+        wrapper.style.fontWeight = this.config.bold ? 'bold' : 'normal'
+        wrapper.style.fontStyle = this.config.italic ? 'italic' : 'normal'
+        wrapper.style.fontFamily = this.config.fontFamily
+        wrapper.style.letterSpacing = this.config.letterSpacing
+        wrapper.style.lineHeight = this.config.lineHeight
+        wrapper.innerHTML = '&nbsp;<span id="it">ES</span>K<span id="is">IST</span>A<span id="five">KURZ</span>&nbsp;<br>' +
+                            '&nbsp;<span id="ten">ZEHN</span><span id="quarter">VIERTEL</span>&nbsp;<br>' +
+                            '&nbsp;<span id="twenty">ZWANZIG</span>SPÄT&nbsp;<br>' +
+                            '&nbsp;<span id="before">VOR</span>NULL<span id="past">NACH</span>&nbsp;<br>' +
+                            '&nbsp;<span id="half">HALB</span>TAU<span id="hour1">EIN</span><span id="einS">S</span>&nbsp;<br>' +
+                            '&nbsp;<span id="hour4">VIER</span>ST<span id="hour12">ZWÖLF</span>&nbsp;<br>' +
+                            '&nbsp;<span id="hour6">SECHS</span><span id="hour7">SIEBEN</span>&nbsp;<br>' +
+                            '&nbsp;<span id="hour3">DREI</span><span id="hour5">FÜNF</span><span id="hour11">ELF</span>&nbsp;<br>' +
+                            '&nbsp;<span id="hour9">NEUN</span>PI<span id="hour2">ZWEI</span>N&nbsp;<br>' +
+                            '&nbsp;<span id="hour8">ACHT</span><span id="hour10">ZEHN</span><span id="full">UHR</span>&nbsp;<br>'
+        return wrapper
+    }
+})
--- README.md
+++ README.md
@@ -1,15 +1,46 @@
-# Module: Word Clock
+# Module: German Word Clock
 This Magic Mirror module displays a Word Clock written in german.
 
-## Using the module
+## Installing
 
-To use this module, add it to the modules array in the `config/config.js` file:
-````javascript
-modules: [
-    {
-		module: 'MMM-germanwordclock',
-		position: 'bottom_left',
-	},
-]
-````
+### Step 1 - Install the module
+```bash
+cd ~/MagicMirror/modules
+git clone https://github.com/nikolasdas/MMM-germanwordclock
+cd MMM-germanwordclock
+npm install
+```
 
+### Step 2 - Add module to `~MagicMirror/config/config.js`
+Add this configuration into `config.js` file's
+```javascript
+{
+	module: 'MMM-germanwordclock',
+	position: 'middle_center',
+	config: {
+		// see below for config options
+	}
+}
+```
+
+## Updating
+Go to the module’s folder inside MagicMirror modules folder and pull the latest version from GitHub and install:
+```bash
+git pull
+npm install
+```
+
+## Configuring
+Here is the configurable part of the module
+
+| Option               | Description
+|--------------------- |-----------
+| `updateInterval` | Update Interval<br>**Type:** `number` in ms<br>**Default value:** `1000`
+| `bold`           | Bold Letters<br>**Type:** `boolean`<br>**Default value:** `true`
+| `italic`         | Italic Letters<br>**Type:** `boolean`<br>**Default value:** `false`
+| `fontFamily`     | Font<br>**Type:** `string`<br>**Default value:** `Courier`
+| `fontSize`       | Font Size<br>**Type:** `length` \| `percentage`<br>**Default value:** `.5rem`
+| `letterSpacing`  | Letter Spacing<br>**Type:** `length`<br>**Default value:** `1rem`
+| `lineHeight`     | Line Height<br>**Type:** `number` \| `length` \| `percentage`<br>**Default value:** `1rem`
+| `color`          | Color of Letters<br>**Type:** `color`<br>**Default value:** `rgba(255,255,255,0.2)`
+| `selectedColor`  | Color of active Letters<br>**Type:** `color`<br>**Default value:** `rgba(255,255,255,1)`
